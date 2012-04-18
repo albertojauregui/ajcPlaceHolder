b189d731f49634a051b0a82e656c305a9b682cd5
Alberto Jauregui <bastertwo@gmail.com>
2012-04-18 15:17:09 -0600

    Update jQuery.ajcPlaceHolder.js

---
+++ jQuery.ajcPlaceHolder.js
@@ -0,0 +1,11 @@
+(function($) {
+  $.fn.ajcPlaceHolder = function() {
+    return this.each(function(){
+    var $this = $(this);
+	  var defvalue = $this.attr("auxvalue") || "";
+	  if($.trim($this.val()) == ""){$this.val(defvalue);}
+	  $this.focus(function() {if($this.val() == defvalue) {$this.val("");}});
+	  $this.blur(function() {if($.trim($this.val()) == "") {$this.val() = defvalue;}});
+    });
+  }
+ })(jQuery);
